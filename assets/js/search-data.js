eaf21d677cd2f65695ff98eedea7b1250a3b5a0e
KevinRoller <92864684+KevinRoller@users.noreply.github.com>
2026-03-28 11:58:44 +0000

    Deploying to gh-pages from @ KevinRoller/KevinRoller.github.io@bdd16df2c949f33a17ee6c09c4a8aae54a140512 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,25 +507,32 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/al-folio/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/al-folio/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%68%6D%74%72%75%6E%67%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-scholar',
+        title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://scholar.google.com/citations?user=Y9fWf3YAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/trung-pham-thanh-432798218", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/KevinRoller", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,20 +540,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/al-folio/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
